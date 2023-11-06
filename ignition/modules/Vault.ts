ae50612c30f9bf9fbc5fb0fbbcecc8bf6f735926
maguro <maguro.abs@gmail.com>
2023-11-06 18:30:24 +0900

    fix: the way of specifying id

--- ignition/modules/Vault.ts
+++ ignition/modules/Vault.ts
@@ -4,21 +4,25 @@ import MockSwapper from './swappers/MockSwapper'
 import VaultFactory from './VaultFactory'
 
 // * NOTES: deployment with factory currently not working as expected.
-// *        deployed address not stored in json file, and visualization throws error
+// *        deployed address not stored in json file.
 export default buildModule('Vault', (m) => {
   const { vaultFactory } = m.useModule(VaultFactory)
   const { uniswapV3Swapper } = m.useModule(UniswapV3Swapper)
   const { mockSwapper } = m.useModule(MockSwapper)
 
   const create1 = m.call(vaultFactory, 'createVault', [m.getParameter('USDC'), uniswapV3Swapper], {
-    id: 'uniswapV3_usdc',
+    id: 'create1',
   })
   const create2 = m.call(vaultFactory, 'createVault', [m.getParameter('USDC'), mockSwapper], {
-    id: 'mock_usdc',
+    id: 'create2',
   })
 
-  const univ3Usdc = m.contractAt('Vault', m.readEventArgument(create1, 'VaultCreated', 'vault', { id: 'create1' }))
-  const mockUsdc = m.contractAt('Vault', m.readEventArgument(create2, 'VaultCreated', 'vault'), { id: 'create2' })
+  const univ3Usdc = m.contractAt('Vault', m.readEventArgument(create1, 'VaultCreated', 'vault', { id: 'read1' }), {
+    id: 'univ3_usdc',
+  })
+  const mockUsdc = m.contractAt('Vault', m.readEventArgument(create2, 'VaultCreated', 'vault', { id: 'read2' }), {
+    id: 'mock_usdc',
+  })
 
   return {
     vaultFactory,
